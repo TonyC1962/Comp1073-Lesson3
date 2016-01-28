bce678271cd5abf9dbe3e30a354fae98e1fda1f0
TonyC1962 <ajcgoldrush@gmail.com>
2016-01-28 10:51:14 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+// main javascript document
+
+"use strict";
+
+var username = "Tony";
+var age= 30;
+var isHungry = true;
+
+var usernames = [];
+
+var usernames = ["Mike", "Rich", "Anju", "Margot"];
+console.log(usernames);
+
+/*
+//var usernames = new Array();
+
+usernames [0] = "Mike";
+usernames [0] = "Rich";
+usernames [0] = "Anju";
+usernames [0] = "Margot";
+
+console.log(usernames);*/
